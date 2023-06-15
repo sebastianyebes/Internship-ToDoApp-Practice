24830508f29a33a2cf67567e9046f6706a2b17fd
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-06-15 16:41:40 +0800

    Implement Delete method for Task

--- todoapp-backend-django/api/views.py
+++ todoapp-backend-django/api/views.py
@@ -12,6 +12,9 @@ class TaskView(APIView):
     @staticmethod
     def get(request):
         tasks = Task.objects.all()
+        if not tasks:
+            return Response("No Content", status=status.HTTP_204_NO_CONTENT)
+
         serializer = TasksSerializer(tasks, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -38,3 +41,9 @@ class TaskDetailView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @staticmethod
+    def delete(request, task_id):
+        task = get_object_or_404(Task, pk=task_id)
+        task.delete()
+        return Response(f"Lesson {task_id} deleted successfully", status=status.HTTP_200_OK)
