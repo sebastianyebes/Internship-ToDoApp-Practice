043de91c668b4dbf3c3bc4b4559962ed2328a0bd
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-06-15 16:21:07 +0800

    Implement getall method of Task

--- todoapp-backend-django/api/views.py
+++ todoapp-backend-django/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Task
+from .serializers import TasksSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
+class TaskView(APIView):
+    @staticmethod
+    def get(request):
+        tasks = Task.objects.all()
+        serializer = TasksSerializer(tasks, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
