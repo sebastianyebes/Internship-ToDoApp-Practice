9ef9ef800a764721166772ca57b78bdc348e3de9
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-06-15 15:56:46 +0800

    Implement Task Model

--- todoapp-backend-django/api/models.py
+++ todoapp-backend-django/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Tasks(models.Model):
+    task = models.TextField(default="", null=False, blank=False)
+
+    def __str__(self):
+        return self.task
