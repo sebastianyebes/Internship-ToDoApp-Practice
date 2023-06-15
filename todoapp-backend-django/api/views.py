53824dbe6620a27ea425db8694487cb1bf97a2e8
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-06-15 16:35:43 +0800

    Implement Put method for Task

--- todoapp-backend-django/api/views.py
+++ todoapp-backend-django/api/views.py
@@ -29,3 +29,12 @@ class TaskDetailView(APIView):
         task = get_object_or_404(Task, pk=task_id)
         serializer = TasksSerializer(task)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @staticmethod
+    def put(request, task_id):
+        task = get_object_or_404(Task, pk=task_id)
+        serializer = TasksSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
