36f06c9ba6d59012180a897524daa36a8c8fa5b1
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-06-15 16:27:19 +0800

    Implement Post method for Task

--- todoapp-backend-django/api/views.py
+++ todoapp-backend-django/api/views.py
@@ -13,3 +13,11 @@ class TaskView(APIView):
         tasks = Task.objects.all()
         serializer = TasksSerializer(tasks, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @staticmethod
+    def post(request):
+        serializer = TasksSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
