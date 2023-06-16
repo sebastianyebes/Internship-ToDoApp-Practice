0a96d66067cfc0f01846f621d956e3661144fc0d
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-06-16 14:47:55 +0800

    Modify homepage

--- todoapp-frontend-nextjs-react/app/page.js
+++ todoapp-frontend-nextjs-react/app/page.js
@@ -1,9 +1,25 @@
-import styles from './page.module.css'
+import './page.css'
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
-    </main>
+    <>
+      <div className='page-title'>
+        <h1>Todo App</h1>
+      </div>
+      <div className='page-tasks'>
+        <ul>
+          <li>tasks</li>
+          <li>tasks</li>
+        </ul>
+      </div>
+      <div className='page-forms'>
+          <form action="">
+            <label htmlFor="task">Input: </label>
+            <input type="text"/>
+            <br />
+            <input type="submit" value="Submit" className='page-submit-button'/>
+          </form>
+      </div>
+    </>
   )
 }
