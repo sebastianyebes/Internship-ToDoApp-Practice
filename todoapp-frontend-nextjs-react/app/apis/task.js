7aa9fca0d68bb1f51cc6b08ded3b925a5c67b142
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-06-19 09:59:05 +0800

    Implement updateTask method

--- todoapp-frontend-nextjs-react/app/apis/task.js
+++ todoapp-frontend-nextjs-react/app/apis/task.js
@@ -15,23 +15,37 @@ export const getTask = async (id) => {
 };
 
 export const getTasks = async () => {
-    return await axios.get(`${baseUrl}api/tasks/`)
-    .then(response => {
-        return response.data;
+  return await axios
+    .get(`${baseUrl}api/tasks/`)
+    .then((response) => {
+      return response.data;
     })
-    .catch(error => {
-        console.log(error);
-        return error;
+    .catch((error) => {
+      console.log(error);
+      return error;
     });
-}
+};
 
 export const createTask = async (data) => {
-    return await axios.post(`${baseUrl}api/tasks/`, data)
-    .then(response => {
-        return response.data;
+  return await axios
+    .post(`${baseUrl}api/tasks/`, data)
+    .then((response) => {
+      return response.data;
     })
-    .catch(error => {
-        console.log(error);
-        return error;
+    .catch((error) => {
+      console.log(error);
+      return error;
     });
-}
+};
+
+export const updateTask = async (id, data) => {
+  return await axios
+    .put(`${baseUrl}api/tasks/${id}/`, data)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+      return error;
+    });
+};
