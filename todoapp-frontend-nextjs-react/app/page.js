b8753ced7a301a517595d3e28d67d05dc8b9047c
Carl Yebes <carlsebastian.yebes@cit.edu>
2023-06-20 13:45:00 +0800

    Commit all

--- todoapp-frontend-nextjs-react/app/page.js
+++ todoapp-frontend-nextjs-react/app/page.js
@@ -1,13 +1,19 @@
 "use client";
 import "./page.css";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchTasks, AddTask } from "./redux/features/task-slice";
+import {
+  fetchTasks,
+  AddTask,
+  removeTask,
+  putTask,
+} from "./redux/features/task-slice";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const distpatch = useDispatch();
   const tasks = useSelector((state) => state.tasks);
   let [input, setInput] = useState("");
+  let [updatedTask, setUpdatedTask] = useState("");
 
   useEffect(() => {
     distpatch(fetchTasks());
@@ -17,9 +23,30 @@ export default function Home() {
     setInput(e.target.value);
   };
 
+  const updateTaskInputHandler = (e) => {
+    setUpdatedTask(e.target.value);
+  };
+
   const createTaskHandler = (e) => {
     distpatch(AddTask({ todo: input }));
     setInput("");
+  };
+
+  const removeTaskHandler = (id) => {
+    distpatch(removeTask(id)).then(() => {
+      distpatch(fetchTasks());
+    });
+  };
+
+  const updateTaskHandler = (id, data) => {
+    const taskToUpdate = tasks.tasks.find((task) => task.id === id);
+    if (taskToUpdate) {
+      const updatedTaskData = { ...taskToUpdate, todo: data };
+      distpatch(putTask({ id, data: updatedTaskData })).then(() => {
+        distpatch(fetchTasks());
+        console.log(tasks);
+      });
+    }
   };
 
   return (
@@ -28,11 +55,30 @@ export default function Home() {
         <h1>Todo App</h1>
       </div>
       <div className="page-tasks">
-        <ul>
-          {tasks?.tasks.map((task) => (
-            <li key={task.id}> {task.todo}</li>
-          ))}
-        </ul>
+        {tasks.tasks.length === 0 ? (
+          <h1>No tasks</h1>
+        ) : (
+          <ul>
+            {tasks.tasks.map((task, count) => (
+              <div className="page-task" key={task.id}>
+                <li>{task.todo}</li>
+                <button
+                  onClick={() => removeTaskHandler(task.id)}
+                  className="task-button"
+                >
+                  Delete
+                </button>
+                <button
+                  onClick={() => updateTaskHandler(task.id, updatedTask)}
+                  className="task-button"
+                >
+                  Update
+                </button>
+                <input type="text" onChange={updateTaskInputHandler} />
+              </div>
+            ))}
+          </ul>
+        )}
       </div>
       <div className="page-forms">
         <label htmlFor="task">Input: </label>
--- todoapp-frontend-nextjs-react/app/redux/features/task-slice.js
+++ todoapp-frontend-nextjs-react/app/redux/features/task-slice.js
@@ -41,6 +41,27 @@ export const AddTask = createAsyncThunk("tasks/createTask", async (data) => {
   }
 });
 
+export const removeTask = createAsyncThunk("tasks/deleteTask", async (id) => {
+  try {
+    const response = await deleteTask(id);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+});
+
+export const putTask = createAsyncThunk(
+  "tasks/updateTask",
+  async ({ id, data }) => {
+    try {
+      const response = await updateTask(id, data);
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+);
+
 const taskSlice = createSlice({
   name: "tasks",
   initialState,
@@ -70,7 +91,42 @@ const taskSlice = createSlice({
         state.error = action.error.message;
       })
       .addCase(AddTask.fulfilled, (state, action) => {
-        state.tasks.push(action.payload);
+        state.loading = false;
+        state.tasks = [...state.tasks, action.payload];
+      })
+      .addCase(AddTask.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(AddTask.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      .addCase(removeTask.fulfilled, (state, action) => {
+        state.loading = false;
+        const deletedTaskId = action.payload.id;
+        state.tasks = state.tasks.filter((task) => task.id !== deletedTaskId);
+      })
+      .addCase(removeTask.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(removeTask.rejected, (state, action) => {
+        state.error = action.error.message;
+      })
+      .addCase(putTask.fulfilled, (state, action) => {
+        state.loading = false;
+        const updatedTask = action.payload;
+        const existingTask = state.tasks.find(
+          (task) => task.id === updatedTask.id
+        );
+        if (existingTask) {
+          existingTask.todo = updatedTask.todo;
+        }
+      })
+      .addCase(putTask.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(putTask.rejected, (state, action) => {
+        state.error = action.error.message;
       });
   },
 });
